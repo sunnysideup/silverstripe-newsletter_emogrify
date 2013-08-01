fb03af14a48a67cf47a30f4a27ec3556d0f28047
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-08-01 10:55:42 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/newsletter_emogrify/trunk@5005 7b40dde9-fc51-43d4-b609-63a66f0a0cee

---
+++ _config.php
@@ -0,0 +1,9 @@
+<?php
+
+//===================---------------- START newsletter MODULE ----------------===================
+
+Email::set_mailer(new EmogrifierMailer());
+//MyMailer::set_css_file("/themes/mytheme/css/emailTypography.css");
+//NewsletterAdmin::$template_paths = array("themes/mytheme/templates/Email");
+//SubscriptionPage::$icon = "mysite/images/treeicons/SubscriptionPage";
+//===================---------------- END newsletter MODULE ----------------===================
