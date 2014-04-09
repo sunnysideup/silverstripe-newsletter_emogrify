7b932f9dabe9c769b6bb2e58a25df2b49aba6eff
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 01:27:16 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/newsletter_emogrify/trunk@5576 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/EmogrifierMailer.php
+++ code/EmogrifierMailer.php
@@ -3,11 +3,10 @@
 class EmogrifierMailer extends Mailer {
 
 	private static $css_file = '';
-		public static function set_css_file($s){ self::$css_file = $s;}
 
 	function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false) {
 		require_once(Director::baseFolder() . '/newsletter_emogrify/thirdparty/Emogrifier.php');
-		$cssFileLocation = Director::baseFolder() . self::$css_file;
+		$cssFileLocation = Director::baseFolder() . Config::inst()->get("EmogrifierMailer", "css_file");
 		$cssFileHandler = fopen($cssFileLocation, 'r');
 		$css = fread($cssFileHandler,  filesize($cssFileLocation));
 		fclose($cssFileHandler);
