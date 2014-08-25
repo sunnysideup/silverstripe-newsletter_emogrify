acc9e81b88b69f85079fd86b2c4f8fe46f244b0c
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-08-25 05:17:59 +0000

    IMPROVEMENT
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/newsletter_emogrify/trunk@5791 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/EmogrifierMailer.php
+++ code/EmogrifierMailer.php
@@ -5,13 +5,16 @@ class EmogrifierMailer extends Mailer {
 	private static $css_file = '';
 
 	function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false) {
-		require_once(Director::baseFolder() . '/newsletter_emogrify/thirdparty/Emogrifier.php');
-		$cssFileLocation = Director::baseFolder() . Config::inst()->get("EmogrifierMailer", "css_file");
-		$cssFileHandler = fopen($cssFileLocation, 'r');
-		$css = fread($cssFileHandler,  filesize($cssFileLocation));
-		fclose($cssFileHandler);
-		$emog = new \Pelago\Emogrifier($htmlContent, $css);
-		$htmlContent = $emog->emogrify();
+		//only emogrify it it has not been emogrified yet...
+		if(!class_exists("\Pelago\Emogrifier")) {
+			require_once(Director::baseFolder() . '/newsletter_emogrify/thirdparty/Emogrifier.php');
+			$cssFileLocation = Director::baseFolder() . Config::inst()->get("EmogrifierMailer", "css_file");
+			$cssFileHandler = fopen($cssFileLocation, 'r');
+			$css = fread($cssFileHandler,  filesize($cssFileLocation));
+			fclose($cssFileHandler);
+			$emog = new \Pelago\Emogrifier($htmlContent, $css);
+			$htmlContent = $emog->emogrify();
+		}
 		return parent::sendHTML($to, $from, $subject, $htmlContent, $attachedFiles, $customheaders, $plainContent);
 	}
 }
